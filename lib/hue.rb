684129308d6f52100f49b27bf7fea5d30616f44d
Patrick Neff <pneff@anpi.com>
2013-08-07 14:51:49 -0500

    Added lib/hue/rgb.rb lib/hue/hexrgb.rb and lib/hue/hsl.rb
    Implements Issue #6
    
    These classes allow you to generate the HUE color values from 3 different formats.
    RGB.new(123,231,222).to_hue
    HexRGB.new("7BD5DE").to_hue
    HSL.new(185.45, 0.60, 0.67).to_hue
    They each return a hash in the format of: {:hue=>33760, :saturation=>153, :luminance=>172}

--- lib/hue.rb
+++ lib/hue.rb
@@ -7,6 +7,9 @@ require 'hue/translate_keys'
 require 'hue/light'
 require 'hue/group'
 require 'hue/scene'
+require 'hue/rgb'
+require 'hue/hsl'
+require 'hue/hexrgb'
 
 module Hue
   USERNAME_RANGE = 10..40
